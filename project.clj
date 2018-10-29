f0f70fd0ee83eddf13b89fb4f1489d581fa7a17c
Leonardo Alves de Melo <leonardo.alves.melo.1995@gmail.com>
2018-10-29 14:42:32 -0300

    Getting temperature passing a city as a parameter

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-  				[clj-http "3.9.1"]]
+  				[clj-http "3.9.1"]
+  				[org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot find-temperature.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/find_temperature/core.clj
+++ src/find_temperature/core.clj
@@ -1,7 +1,44 @@
 (ns find-temperature.core
   (:gen-class))
 
+(ns find-temperature.core
+  (:require [clj-http.client :as client]))
+
+(ns find-temperature.core
+  (:require [clojure.data.json :as json]))
+
+(require '[clj-http.client :as http])
+
+(defn get_temperature [city] 
+	(let [api_url "http://api.openweathermap.org/data/2.5/weather?q="
+		app_id "&APPID=173c44713662655df575fc4200bbb202"] 
+
+		(client/get (str api_url city app_id)
+            {:async? true :as :json}
+            ;; respond callback
+            (fn [response] 
+            	(println "Temperature is:" 
+            		(:temp
+	            		(:main
+	            			(json/read-str 
+	            				(:body response) :key-fn keyword
+	            			)
+	            			
+	            		)
+            		)
+
+            		"ºF"
+
+            	)
+            )
+            ;; raise callback
+            (fn [exception] (println "exception message is: " (.getMessage exception) city))
+    	)
+	)
+	
+)
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (get_temperature (first args))
+ )
