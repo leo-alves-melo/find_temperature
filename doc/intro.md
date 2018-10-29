4ca12077323527dafcbd9e676ad46325ee4262e2
Leonardo Alves de Melo <leonardo.alves.melo.1995@gmail.com>
2018-10-29 13:42:32 -0300

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to find_temperature
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/find_temperature/core_test.clj
@@ -0,0 +1,7 @@
+(ns find-temperature.core-test
+  (:require [clojure.test :refer :all]
+            [find-temperature.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
